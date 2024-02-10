3862ce4936504cac85979ec65dd88e1d5aae72d2
francisconeto00 <franciscodgneto@gmail.com>
2024-02-10 19:45:04 +0000

    otj

---
+++ OTJ/src/api/base-de-dado/routes/base-de-dado.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * base-de-dado router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::base-de-dado.base-de-dado');
---
+++ OTJ/src/api/base-de-dado/services/base-de-dado.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * base-de-dado service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::base-de-dado.base-de-dado');
